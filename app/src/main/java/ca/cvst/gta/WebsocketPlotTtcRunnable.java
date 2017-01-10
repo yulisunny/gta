b64c70f3969868163e9d8698901b219d835eb527
Harry Yu <harryyu1994@gmail.com>
2017-01-10 18:37:29 -0500

    added animation to ttc markers

--- app/src/main/java/ca/cvst/gta/WebsocketPlotTtcRunnable.java
+++ app/src/main/java/ca/cvst/gta/WebsocketPlotTtcRunnable.java
@@ -1,8 +1,14 @@
 package ca.cvst.gta;
 
 import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.view.animation.Interpolator;
+import android.view.animation.LinearInterpolator;
 
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.Projection;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
@@ -70,8 +76,8 @@ public class WebsocketPlotTtcRunnable implements Runnable {
 
                 JSONArray coordinates = data.getJSONArray("coordinates");
                 LatLng location = new LatLng(coordinates.getDouble(1), coordinates.getDouble(0));
-                m.setPosition(location);
-
+                //m.setPosition(location);
+                animateMarker(m, location, false);
                 String direction = Helper.calculateDirection(Integer.parseInt(data.getString("heading")));
 
                 String dateTime = Helper.convertTimestampToString(data.getLong("GPStime"));
@@ -99,5 +105,42 @@ public class WebsocketPlotTtcRunnable implements Runnable {
         } catch (JSONException e) {
             e.printStackTrace();
         }
+    }
+
+    public void animateMarker(final Marker marker, final LatLng toPosition,
+                              final boolean hideMarker) {
+        final Handler handler = new Handler();
+        final long start = SystemClock.uptimeMillis();
+        Projection proj = mMap.getProjection();
+        Point startPoint = proj.toScreenLocation(marker.getPosition());
+        final LatLng startLatLng = proj.fromScreenLocation(startPoint);
+        final long duration = 500;
+
+        final Interpolator interpolator = new LinearInterpolator();
+
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                long elapsed = SystemClock.uptimeMillis() - start;
+                float t = interpolator.getInterpolation((float) elapsed
+                        / duration);
+                double lng = t * toPosition.longitude + (1 - t)
+                        * startLatLng.longitude;
+                double lat = t * toPosition.latitude + (1 - t)
+                        * startLatLng.latitude;
+                marker.setPosition(new LatLng(lat, lng));
+
+                if (t < 1.0) {
+                    // Post again 16ms later.
+                    handler.postDelayed(this, 16);
+                } else {
+                    if (hideMarker) {
+                        marker.setVisible(false);
+                    } else {
+                        marker.setVisible(true);
+                    }
+                }
+            }
+        });
     }
 }
