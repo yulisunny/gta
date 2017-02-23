fea855e32f112ca7685fd476c06372cdf9de71f5
Susan Dang <rsusandang@gmail.com>
2017-02-23 15:19:07 -0500

    Add in ability to search for linkIds near an address

--- app/src/main/java/ca/cvst/gta/NewHistoricalHighwayMapActivity.java
+++ app/src/main/java/ca/cvst/gta/NewHistoricalHighwayMapActivity.java
@@ -1,13 +1,17 @@
 package ca.cvst.gta;
 
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
+import com.github.mikephil.charting.data.Entry;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -23,8 +27,14 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class NewHistoricalHighwayMapActivity extends AppCompatActivity implements OnMapReadyCallback {
@@ -58,6 +68,15 @@ public class NewHistoricalHighwayMapActivity extends AppCompatActivity implement
             }
         });
 
+        // Search Button
+        Button searchBtn = (Button)findViewById(R.id.btn_map_new_historical_chart_search);
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onSearch(v);
+            }
+        });
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map_new_historical_chart);
         mapFragment.getMapAsync(this);
@@ -65,8 +84,36 @@ public class NewHistoricalHighwayMapActivity extends AppCompatActivity implement
         getLinkIdInfo();
     }
 
+    public void onSearch(View view) {
+        EditText location = (EditText)findViewById(R.id.historical_map_address_input);
+        String inputLocation = location.getText().toString();
+        List<Address> addressList = null;
+        if (location != null || location.equals("")) {
+            Geocoder geocoder = new Geocoder(this);
+            try {
+                addressList = geocoder.getFromLocationName(inputLocation, 1);
+                System.out.println("FOUND THIS " + addressList.toString());
+            } catch (IOException e) {
+                System.out.println("Exception while fetching geocode for searched location.");
+            }
+            Address address = addressList.get(0);
+            LatLng latlng = new LatLng(address.getLatitude(), address.getLongitude());
+
+
+            List<String> closestLinkIds = getClosestLinkIds(latlng);
+            for(String linkId:closestLinkIds) {
+                Marker marker = mMap.addMarker(new MarkerOptions()
+                        .position(linkIdCoorMapping.get(linkId)));
+                marker.setTag(linkId);
+            }
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(latlng));
+        }
+    }
+
     @Override
     public void onMapReady(final GoogleMap googleMap) {
+        mMap = googleMap;
+
         googleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
             @Override
             public boolean onMarkerClick(Marker clickedMarker) {
@@ -88,12 +135,15 @@ public class NewHistoricalHighwayMapActivity extends AppCompatActivity implement
             }
         });
 
-        // Place markers
-        for (Map.Entry<String, LatLng> entry : linkIdCoorMapping.entrySet()) {
-            Marker marker = googleMap.addMarker(new MarkerOptions()
-                    .position(entry.getValue()));
-            marker.setTag(entry.getKey());
-        }
+//        // Find closest links
+//        getClosestLinkIds()
+
+//        // Place markers
+//        for (Map.Entry<String, LatLng> entry : linkIdCoorMapping.entrySet()) {
+//            Marker marker = googleMap.addMarker(new MarkerOptions()
+//                    .position(entry.getValue()));
+//            marker.setTag(entry.getKey());
+//        }
 
     }
 
@@ -128,16 +178,31 @@ public class NewHistoricalHighwayMapActivity extends AppCompatActivity implement
 
     }
 
-    public class LinkItem implements ClusterItem {
-        private final LatLng mPosition;
+    private List<String> getClosestLinkIds(LatLng desiredLocation) {
+        List<java.util.Map.Entry<String, Double>> pairList= new ArrayList<>();
+        Double x = desiredLocation.latitude;
+        Double y = desiredLocation.longitude;
 
-        public LinkItem(double lat, double lng) {
-            mPosition = new LatLng(lat, lng);
+        for(Map.Entry<String, LatLng> entry:linkIdCoorMapping.entrySet()) {
+            LatLng currPoint = entry.getValue();
+            Double distance = Math.pow(currPoint.latitude - x, 2) + Math.pow(currPoint.longitude - y, 2);
+            pairList.add(new AbstractMap.SimpleEntry<>(entry.getKey(), distance));
         }
 
+        Collections.sort(pairList, new LinkComparator());
+
+        List<String> ret = new ArrayList<>();
+        for (int i = 0; i < 10; i ++) {
+            ret.add(pairList.get(i).getKey());
+        }
+        return ret;
+    }
+
+    public class LinkComparator implements Comparator<Map.Entry<String, Double>> {
+
         @Override
-        public LatLng getPosition() {
-            return mPosition;
+        public int compare(Map.Entry<String, Double> o1, Map.Entry<String, Double> o2) {
+            return o1.getValue().compareTo(o2.getValue());
         }
     }
 }
